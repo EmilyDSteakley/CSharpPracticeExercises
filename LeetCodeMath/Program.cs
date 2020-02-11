57e3b964c526b39c35d96b59e10bd764407b76a8
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-02-11 12:01:09 -0800

    Update

--- LeetCodeMath/Program.cs
+++ LeetCodeMath/Program.cs
@@ -14,16 +14,17 @@ namespace LeetCodeMath
             Console.WriteLine("1295. Find Numbers with Even Number of Digits");
             var _1295_example1 = new int[] { 12, 345, 2, 6, 7896 };
             var _1295_example2 = new int[] { 555, 901, 482, 1771 };
-            Console.WriteLine($"Example 1: {FindNumbersWithEvenNumberOfDigits.FindNumbers(_1295_example1)}");
-            Console.WriteLine($"Example 2: {FindNumbersWithEvenNumberOfDigits.FindNumbers(_1295_example2)}\n\n");
+            var numberFinder = new FindNumbersWithEvenNumberOfDigits();
+            Console.WriteLine($"Example 1: {numberFinder.FindNumbers(_1295_example1)}");
+            Console.WriteLine($"Example 2: {numberFinder.FindNumbers(_1295_example2)}\n\n");
             // Solved January 31, 2020
 
 
             // 1281. Subtract the Product and Sum of Digits of an Integer
             Console.WriteLine("1281. Subtract the Product and Sum of Digits of an Integer");
             var productSumCalc = new SubtractTheProductAndSumOfDigitsOfAnInteger();
-            Console.WriteLine($"Example 1: {productSumCalc.SubtractProductAndSum24(234)}");
-            Console.WriteLine($"Example 2: {productSumCalc.SubtractProductAndSum24(35252)}");
+            Console.WriteLine($"Example 1: {productSumCalc.SubtractProductAndSum(234)}");
+            Console.WriteLine($"Example 2: {productSumCalc.SubtractProductAndSum(35252)}");
             // Solved January 31, 2020
         }
     }
