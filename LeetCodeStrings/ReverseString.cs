3e7c85a07e59970aba5fe74b47751d34dc06d756
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-02-11 11:50:21 -0800

    Solved February 10, 2020

--- LeetCodeStrings/ReverseString.cs
+++ LeetCodeStrings/ReverseString.cs
@@ -8,29 +8,28 @@ namespace LeetCodeStrings
 {
     class ReverseString
     {
-        public static void ReverseStrings(char[] s)
+        // ---------------------------------------- Problem ----------------------------------------
+
+        // 344. Reverse String
+        // Write a function that reverses a string. The input string is given as an array of characters char[].
+
+        // Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+        // You may assume all the characters consist of printable ascii characters.
+
+        // Example 1:
+        // Input: ["h","e","l","l","o"]
+        // Output: ["o","l","l","e","h"]
+
+        // Example 2:
+        // Input: ["H","a","n","n","a","h"]
+        // Output: ["h","a","n","n","a","H"]
+
+
+
+        // ---------------------------------------- My Solution ----------------------------------------
+        public void ReverseStrings(char[] s)
         {
-            // ---------------------------------------- Problem ----------------------------------------
-
-            // 344. Reverse String
-            // Write a function that reverses a string. The input string is given as an array of characters char[].
-
-            // Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
-
-            // You may assume all the characters consist of printable ascii characters.
-
-            // Example 1:
-            // Input: ["h","e","l","l","o"]
-            // Output: ["o","l","l","e","h"]
-
-            // Example 2:
-            // Input: ["H","a","n","n","a","h"]
-            // Output: ["h","a","n","n","a","H"]
-
-
-
-            // ---------------------------------------- My Solution ----------------------------------------
-
             var a = s.Length - 1;
 
             for (int i = 0; i <= a; i++)
@@ -44,11 +43,12 @@ namespace LeetCodeStrings
 
             // 392 ms <-- Beats 45.34%
             // Solved February 10, 2020
+        }
 
 
-
-            // ---------------------------------------- 348 ms Solution on LeetCode ----------------------------------------
-
+        // ---------------------------------------- 348 ms Solution on LeetCode ----------------------------------------
+        public void ReverseString348(char[] s)
+        {
             for (int i = 0, j = s.Length - 1; i < j; i++, j--)
             {
                 char c = s[i];
