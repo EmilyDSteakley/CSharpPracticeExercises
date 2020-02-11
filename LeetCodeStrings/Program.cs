b512d19752055659f75fb1ec2ed289cee9d3bfd7
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-02-10 20:38:00 -0800

    Update

--- LeetCodeStrings/Program.cs
+++ LeetCodeStrings/Program.cs
@@ -10,6 +10,13 @@ namespace LeetCodeStrings
     {
         static void Main(string[] args)
         {
+            // 344. Reverse String
+            Console.WriteLine("344. Reverse String");
+            var example1 = new[] { 'h', 'e', 'l', 'l', 'o' };
+            var example2 = new[] { 'H', 'a', 'n', 'n', 'a', 'h' };
+            ReverseString.ReverseStrings(example1);
+            ReverseString.ReverseStrings(example2);
+            // Solved February 10, 2020
         }
     }
 }
