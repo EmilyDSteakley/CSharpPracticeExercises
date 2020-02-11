68393a010040ad301400bb78ac5bed41a1f43a7a
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-02-11 11:55:28 -0800

    Solved January 31, 2020

--- LeetCodeMath/SubtractTheProductAndSumOfDigitsOfAnInteger.cs
+++ LeetCodeMath/SubtractTheProductAndSumOfDigitsOfAnInteger.cs
@@ -8,36 +8,35 @@ namespace LeetCodeMath
 {
     class SubtractTheProductAndSumOfDigitsOfAnInteger
     {
-        public static int SubtractProductAndSum(int n)
+        // ---------------------------------------- Problem ----------------------------------------
+
+        // 1281. Subtract the Product and Sum of Digits of an Integer
+        // Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+
+        // Example 1:
+        // Input: n = 234
+        // Output: 15
+        // Explanation:
+        // Product of digits = 2 * 3 * 4 = 24
+        // Sum of digits = 2 + 3 + 4 = 9
+        // Result = 24 - 9 = 15
+
+        // Example 2:
+        // Input: n = 4421
+        // Output: 21
+        // Explanation:
+        // Product of digits = 4 * 4 * 2 * 1 = 32
+        // Sum of digits = 4 + 4 + 2 + 1 = 11
+        // Result = 32 - 11 = 21
+
+        // Constraints:
+        // 1 <= n <= 10 ^ 5
+
+
+
+        // ---------------------------------------- My Solution ----------------------------------------
+        public int SubtractProductAndSum(int n)
         {
-            // ---------------------------------------- Problem ----------------------------------------
-
-            // 1281. Subtract the Product and Sum of Digits of an Integer
-            // Given an integer number n, return the difference between the product of its digits and the sum of its digits.
-
-            // Example 1:
-            // Input: n = 234
-            // Output: 15
-            // Explanation:
-            // Product of digits = 2 * 3 * 4 = 24
-            // Sum of digits = 2 + 3 + 4 = 9
-            // Result = 24 - 9 = 15
-
-            // Example 2:
-            // Input: n = 4421
-            // Output: 21
-            // Explanation:
-            // Product of digits = 4 * 4 * 2 * 1 = 32
-            // Sum of digits = 4 + 4 + 2 + 1 = 11
-            // Result = 32 - 11 = 21
-
-            // Constraints:
-            // 1 <= n <= 10 ^ 5
-
-
-
-            // ---------------------------------------- My Solution ----------------------------------------
-
             // 1. Convert int to byte array
             var numbers = new Stack<byte>();
 
@@ -68,10 +67,11 @@ namespace LeetCodeMath
 
             // 40 ms <-- Beats 73.98%
             // Solved January 31, 2020
+        }
 
-
-
-            // ---------------------------------------- 24 ms Solution on LeetCode ----------------------------------------
+        // ---------------------------------------- 24 ms Solution on LeetCode ----------------------------------------
+        public int SubtractProductAndSum24(int n)
+        {
 
             string nums = n.ToString();
             int product = 1;
