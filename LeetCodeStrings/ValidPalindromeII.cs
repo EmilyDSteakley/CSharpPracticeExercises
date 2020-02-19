82bb5ef24963e5de3e5b69ede507b8b75ffd5e35
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-02-19 14:54:21 -0800

    Create ValidPalindromeII.cs

---
+++ LeetCodeStrings/ValidPalindromeII.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeStrings
+{
+    // ---------------------------------------- Problem ----------------------------------------
+
+    // 680. Valid Palindrome II
+    // Given a non-empty string s, you may delete at most one character.Judge whether you can make it a palindrome.
+
+    // Example 1:
+    // Input: "aba"
+    // Output: True
+
+    // Example 2:
+    // Input: "abca"
+    // Output: True
+    // Explanation: You could delete the character 'c'.
+
+    // Note:
+    // 1. The string will only contain lowercase characters a-z.The maximum length of the string is 50000.
+
+
+
+    // ---------------------------------------- My Solution ----------------------------------------
+    class ValidPalindromeII
+    {
+
+    }
+    // 40 ms <-- Beats 73.98%
+    // Solved January 31, 2020
+
+
+
+    // ---------------------------------------- 24 ms Solution on LeetCode ----------------------------------------
+
+
+
+} 
