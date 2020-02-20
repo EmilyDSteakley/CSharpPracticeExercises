4c7b3bc66fb7e8f5ffa6d522150dbec6f288f40d
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-02-20 13:31:00 -0800

    Update

--- Program.cs
+++ Program.cs
@@ -10,15 +10,12 @@ namespace CSharpPracticeExercises
     {
         static void Main(string[] args)
         {
-            var n1 = 7;
-            var ar1 = new[] { 1, 2, 1, 2, 3, 2 };
+            
 
-            var n2 = 9;
-            var ar2 = new[] { 10, 20, 20, 10, 10, 30, 50, 10, 20};
 
-            var merchant = new HR_W_SockMerchant();
-            Console.WriteLine(merchant.sockMerchant(n1, ar1));
-            Console.WriteLine(merchant.sockMerchant(n2, ar2));
+            var counter = new HR_W_CountingValleys();
+            Console.WriteLine(counter.countingValleys(n1, s1));
+            Console.WriteLine(counter.countingValleys(n2, s2));
 
 
 
