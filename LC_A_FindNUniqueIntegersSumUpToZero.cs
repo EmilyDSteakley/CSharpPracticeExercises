c63f2fbd2eaf240449eebab414c1017591579474
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-02-21 17:01:46 -0800

    Create LC_A_FindNUniqueIntegersSumUpToZero.cs

---
+++ LC_A_FindNUniqueIntegersSumUpToZero.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPracticeExercises
+{
+    class LC_A_FindNUniqueIntegersSumUpToZero
+    {
+        // ---------------------------------------- Problem ----------------------------------------
+
+        // 1204. Find N Unique Integers Sum up to Zero
+        // Given an integer n, return any array containing n unique integers such that they add up to 0.
+
+        // Example 1:
+        // Input: n = 5
+        // Output: [-7,-1,1,3,4]
+        // Explanation: These arrays also are accepted[-5, -1, 1, 2, 3] , [-3,-1,2,-2,4].
+        
+        // Example 2:
+        // Input: n = 3
+        // Output: [-1,0,1]
+
+        // Example 3:
+        // Input: n = 1
+        // Output: [0]
+
+        // Constraints:
+        // 1 <= n <= 1000      
+
+
+
+        // ---------------------------------------- My Solution ----------------------------------------
+        public int[] SumZero(int n)
+        {
+
+
+
+
+        }
+            // XXX ms <-- Beats XX.XX%
+            // Solved February XX, 2020
+
+
+        // ---------------------------------------- XXX ms Solution on LeetCode ----------------------------------------
+
+
+
+
+    } // LC_A_FindNUniqueIntegersSumUpToZero class
+
+} // CSharpPracticeExercises namespace
