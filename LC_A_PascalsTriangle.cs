a9736a75ebda322ab879876fa8f5e64a07dd72e5
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-02-24 13:54:19 -0800

    Create LC_A_PascalsTriangle.cs

---
+++ LC_A_PascalsTriangle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPracticeExercises
+{
+    class LC_A_PascalsTriangle
+    {
+        // ---------------------------------------- Problem ----------------------------------------
+
+        // 118. Pascal's Triangle
+        // Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
+
+        // In Pascal's triangle, each number is the sum of the two numbers directly above it.
+
+        // Example:
+
+        // Input: 5
+        // Output:
+        // [
+        //      [1],
+        //     [1,1],
+        //    [1,2,1],
+        //   [1,3,3,1],
+        //  [1,4,6,4,1]
+        // ]
+
+
+
+        // ---------------------------------------- My Solution ----------------------------------------
+        //public IList<IList<int>> Generate(int numRows)
+        //{
+
+
+
+
+
+        //}
+        // XXX ms <-- Beats XX.XX%
+        // Solved February XX, 2020
+
+
+
+        // ---------------------------------------- XXX ms Solution on LeetCode ----------------------------------------
+
+
+    } // LC_A_PascalsTriangle class
+
+} // CSharpPracticeExercises namespace
