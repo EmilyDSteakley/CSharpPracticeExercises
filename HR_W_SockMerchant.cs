3fa0d9dfd6c1a351ada9b786f854553b47c2cd07
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-02-24 14:33:38 -0800

    Solved February 20, 2020

--- HR_W_SockMerchant.cs
+++ HR_W_SockMerchant.cs
@@ -10,7 +10,8 @@ namespace CSharpPracticeExercises
     {
         // ---------------------------------------- Problem ----------------------------------------
 
-        // Sock Merchant
+        // Warm-up Challenges - Sock Merchant
+
         // John works at a clothing store. He has a large pile of socks that he must pair by color for sale. 
         // Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
 
