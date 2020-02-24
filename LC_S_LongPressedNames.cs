aaea1618d90309e3a2e754af24842bcd259ff991
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-02-24 13:54:49 -0800

    Update LC_S_LongPressedNames.cs

--- LC_S_LongPressedNames.cs
+++ LC_S_LongPressedNames.cs
@@ -8,6 +8,40 @@ namespace CSharpPracticeExercises
 {
     class LC_S_LongPressedNames
     {
+        // ---------------------------------------- Problem ----------------------------------------
 
-    }
-}
+        // 925. Long Pressed Name
+
+        // Your friend is typing his name into a keyboard. Sometimes, when typing a character c, 
+        // the key might get long pressed, and the character will be typed 1 or more times.
+
+        // You examine the typed characters of the keyboard. Return True if it is possible that it was 
+        // your friends name, with some characters (possibly none) being long pressed.
+
+
+
+        // Example:
+        // Given nums = [2, 7, 11, 15], target = 9,
+
+        // Because nums[0] + nums[1] = 2 + 7 = 9,
+        // return [0, 1].
+
+
+
+        // ---------------------------------------- My Solution ----------------------------------------
+        public bool IsLongPressedName(string name, string typed)
+        {
+
+
+            return false;
+        }
+        // XXX ms <-- Beats XX.XX%
+        // Solved February XX, 2020
+
+
+
+        // ---------------------------------------- 400 ms Solution on LeetCode ----------------------------------------
+
+    } // LC_S_LongPressedNames
+
+} // CSharpPracticeExercises namespace
