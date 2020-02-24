9e3c366028f861a1177d366a6d823987d6d76ad9
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-02-24 14:08:06 -0800

    Solved February 18, 2020

--- LC_A_TwoSum.cs
+++ LC_A_TwoSum.cs
@@ -11,6 +11,7 @@ namespace CSharpPracticeExercises
         // ---------------------------------------- Problem ----------------------------------------
 
         // 1. Two Sum
+
         // Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
         // You may assume that each input would have exactly one solution, and you may not use the same element twice.
