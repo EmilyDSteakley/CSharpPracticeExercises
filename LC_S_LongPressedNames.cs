2271612875442e53b12d5674221e72849bf1a679
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-02-25 19:54:36 -0800

    Update LC_S_LongPressedNames.cs

--- LC_S_LongPressedNames.cs
+++ LC_S_LongPressedNames.cs
@@ -18,14 +18,29 @@ namespace CSharpPracticeExercises
         // You examine the typed characters of the keyboard. Return True if it is possible that it was 
         // your friends name, with some characters (possibly none) being long pressed.
 
+        // Example 1:
+        // Input: name = "alex", typed = "aaleex"
+        // Output: true
+        // Explanation: 'a' and 'e' in 'alex' were long pressed.
 
+        // Example 2:
+        // Input: name = "saeed", typed = "ssaaedd"
+        // Output: false
+        // Explanation: 'e' must have been pressed twice, but it wasn't in the typed output.
 
-        // Example:
-        // Given nums = [2, 7, 11, 15], target = 9,
+        // Example 3:
+        // Input: name = "leelee", typed = "lleeelee"
+        // Output: true
 
-        // Because nums[0] + nums[1] = 2 + 7 = 9,
-        // return [0, 1].
+        // Example 4:
+        // Input: name = "laiden", typed = "laiden"
+        // Output: true
+        // Explanation: It's not necessary to long press any character.
 
+        // Note:
+        //  1. name.length <= 1000
+        //  2. typed.length <= 1000
+        //  3. The characters of name and typed are lowercase letters.
 
 
         // ---------------------------------------- My Solution ----------------------------------------
