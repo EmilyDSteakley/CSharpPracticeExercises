a4868c9324819f805014d6d1b8eb889da02e72ac
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-02-27 06:23:48 -0800

    Solved February 20, 2020

--- HR_W_SockMerchant.cs
+++ HR_W_SockMerchant.cs
@@ -19,7 +19,9 @@ namespace CSharpPracticeExercises
         // here are three odd socks left, one of each color. The number of pairs is 2.
 
         // Function Description
-        // Complete the sockMerchant function in the editor below. It must return an integer representing the number of matching pairs of socks that are available.
+        // Complete the sockMerchant function in the editor below. 
+        // It must return an integer representing the number of matching pairs of socks that are available.
+        
         // sockMerchant has the following parameter(s):
         //  - n: the number of socks in the pile
         //  - ar: the colors of each sock
