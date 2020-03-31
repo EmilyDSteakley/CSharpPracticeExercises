90c45d49dd0deb2b7cea0a100af94cefa9cda247
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-03-30 17:11:52 -0700

    Solved March 30, 2020

---
+++ Interview_ReverseWordsInString.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPracticeExercises
+{
+    class Interview_ReverseWordsInString
+    {
+        // ---------------------------------------- Problem ----------------------------------------
+
+        // Write a function to reverse words in a string
+        // Example:
+        // Input: I love to code
+        // Output: code to love I
+
+
+
+        // ---------------------------------------- My Solution ----------------------------------------
+
+        public void ReverseWords(string s)
+        {
+            string[] words = s.Split();
+            string[] output = new string[words.Length];
+
+            int position = words.Length - 1;
+            foreach (string word in words)
+            {
+                output[position] = word;
+                position--;
+            }
+            Console.WriteLine(string.Join(" ", output));
+        }
+
+        // Don't use Split() method
+        public void ReverseWordsNoSplit(string s)
+        {
+            List<string> words = new List<string>();
+            StringBuilder temp = new StringBuilder();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] != ' ')
+                    temp.Append(s[i]);
+                else
+                {
+                    words.Add(temp.ToString());
+                    temp.Clear();
+                }
+
+                if (i == s.Length - 1)
+                    words.Add(temp.ToString());
+            }
+
+            string[] output = new string[words.Count];
+
+            int position = words.Count - 1;
+            foreach (string word in words)
+            {
+                output[position] = word;
+                position--;
+            }
+            Console.WriteLine(string.Join(" ", output));
+        }
+    
+    } // Interview_ReverseWordsInString class
+
+} // CSharpPracticeExercises namespace 
---
+++ Interview_SplitArrayIntoTwoEqualSums.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPracticeExercises
+{
+    class Interview_SplitArrayIntoTwoEqualSums
+    {
+        // ---------------------------------------- Problem ----------------------------------------
+
+        // Given an integer array, split the array into two parts such that the sum of the elements in each part is the same
+        // Return the index of the split
+        // Return -1 if it is not possibl
+        // You cannot reorder the integers in the array
+
+        // Example 1:
+        // Input: {2, 4, 4, 5, 5}
+        // Output: 3
+        // Explanation: 2 + 4 + 4 = 10
+        //              5 + 5 = 10
+
+        // Example 2:
+        // Input: {1, 1, 2, 2}
+        // Ouput: -1
+
+
+
+        // ---------------------------------------- My Solution ----------------------------------------
+        
+        // This only works when half is positive...
+        public int SplitArray(int[] arr)
+        {
+            var total = 0;
+            foreach (int num in arr)
+                total += num;
+            
+            if (total % 2 != 0)
+                return -1;
+            else
+            {
+                var half = total / 2;
+                var first = 0;
+                var position = 0;
+                while (first != half && first < half)
+                {
+                    first += arr[position];
+                    position++;
+                }
+                if (first == half)
+                    return position++;
+                else return -1;
+            }
+        }
+
+        // Complete solution
+        public int SplitArrayComplete(int[] arr)
+        {
+            var total = 0;
+            foreach (int num in arr)
+                total += num;
+
+            var left = 0;
+            var leftTotal = 0;
+            var right = arr.Length - 1;
+            var rightTotal = 0;
+
+            if (total / 2 > 0)
+            {
+                while (left <= right)
+                {
+                    if (leftTotal < rightTotal)
+                    {
+                        leftTotal += arr[left++];
+                    }
+                    else
+                    {
+                        rightTotal += arr[right--];
+                    }
+                }
+                if (leftTotal == rightTotal)
+                    return arr.Length - right;
+                else
+                    return -1;
+            }
+            else
+            {
+                while (left <= right)
+                {
+                    if (leftTotal > rightTotal)
+                    {
+                        leftTotal += arr[left++];
+                    }
+                    else
+                    {
+                        rightTotal += arr[right--];
+                    }
+                }
+                if (leftTotal == rightTotal)
+                    return arr.Length - right;
+                else
+                    return -1;
+            }
+        }
+
+
+    } // Interview_SplitArrayIntoTwoEqualSums class
+
+} // CSharpPracticeExercises namespace
