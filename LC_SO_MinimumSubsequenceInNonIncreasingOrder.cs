5506d47f3da9606f43ee189eea08dab1301317ff
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-04-06 14:03:30 -0700

    Solved April 4, 2020

--- LC_SO_MinimumSubsequenceInNonIncreasingOrder.cs
+++ LC_SO_MinimumSubsequenceInNonIncreasingOrder.cs
@@ -8,5 +8,95 @@ namespace CSharpPracticeExercises
 {
     class LC_SO_MinimumSubsequenceInNonIncreasingOrder
     {
+        // ---------------------------------------- Problem ----------------------------------------
+
+        // 1403. Minimum Subsequence in Non-Increasing Order
+
+        // Given the array nums, obtain a subsequence of the array whose sum of elements is strictly greater than the sum of the non included elements in such subsequence. 
+
+        // If there are multiple solutions, return the subsequence with minimum size and 
+        // if there still exist multiple solutions, return the subsequence with the maximum total sum of all its elements. 
+        // A subsequence of an array can be obtained by erasing some(possibly zero) elements from the array. 
+
+        // Note that the solution with the given constraints is guaranteed to be unique. Also return the answer sorted in non - increasing order.
+
+        // Example 1:
+        // Input: nums = [4, 3, 10, 9, 8]
+        // Output: [10, 9]
+        // Explanation: The subsequences[10, 9] and[10, 8] are minimal such that the sum of their elements is strictly greater than the sum of elements not included, 
+        // however, the subsequence[10, 9] has the maximum total sum of its elements. 
+
+        // Example 2:
+        // Input: nums = [4, 4, 7, 6, 7]
+        // Output: [7, 7, 6]
+        // Explanation: The subsequence[7, 7] has the sum of its elements equal to 14 which is not strictly greater than the sum of elements not included(14 = 4 + 4 + 6). 
+        // Therefore, the subsequence[7, 6, 7] is the minimal satisfying the conditions.Note the subsequence has to returned in non - decreasing order.
+
+        // Example 3:
+        // Input: nums = [6]
+        // Output: [6]
+
+        // Constraints:
+        //  - 1 <= nums.length <= 500
+        //  - 1 <= nums[i] <= 100
+
+
+
+        // ---------------------------------------- My Solution ----------------------------------------
+        public IList<int> MinSubsequence(int[] nums)
+        {
+            var output = new List<int>();
+            var total = 0;
+            var subTotal = 0;
+
+            foreach (int n in nums)
+                total += n;
+
+            Array.Sort(nums);
+            var pos = nums.Length - 1;
+            while (subTotal <= total)
+            {
+                output.Add(nums[pos]);
+                total -= nums[pos];
+                subTotal += nums[pos];
+                pos--;
+            }
+
+            return output;
+        }
+        // 248 ms <-- Beats 79.35%
+        // Solved April 4, 2020
+
+
+
+        // ---------------------------------------- 236 ms Solution on LeetCode ----------------------------------------
+        public IList<int> MinSubsequence236(int[] nums)
+        {
+            var res = new List<int>();
+            var record = new int[nums.Max(num => num) + 1];
+            foreach (var num in nums)
+                record[num]++;
+            int sum = nums.Sum(num => num);
+            int subSum = 0;
+            var enough = false;
+            for (int i = record.Length - 1; i >= 0; i--)
+            {
+                if (enough)
+                    break;
+                if (record[i] == 0) continue;
+                for (int j = 0; j < record[i]; j++)
+                {
+                    if (subSum > sum)
+                    {
+                        enough = true;
+                        break;
+                    }
+                    res.Add(i);
+                    subSum += i;
+                    sum -= i;
+                }
+            }
+            return res;
+        }
     }
 }
--- LC_S_NumberOfStepsToReduceANumberInBinaryRepresentationToOne.cs
+++ LC_S_NumberOfStepsToReduceANumberInBinaryRepresentationToOne.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,141 @@ namespace CSharpPracticeExercises
 {
     class LC_S_NumberOfStepsToReduceANumberInBinaryRepresentationToOne
     {
+        // ---------------------------------------- Problem ----------------------------------------
+
+        // 1404. Number of Steps to Reduce a Number in Binary Representation to One
+
+        // Given a number s in their binary representation. 
+        // Return the number of steps to reduce it to 1 under the following rules:
+        //  - If the current number is even, you have to divide it by 2.
+        //  - If the current number is odd, you have to add 1 to it.
+
+        // It's guaranteed that you can always reach to one for all testcases.
+
+        // Example 1:
+        // Input: s = "1101"
+        // Output: 6
+        // Explanation: "1101" corressponds to number 13 in their decimal representation.
+        //  Step 1) 13 is odd, add 1 and obtain 14.
+        //  Step 2) 14 is even, divide by 2 and obtain 7.
+        //  Step 3) 7 is odd, add 1 and obtain 8.
+        //  Step 4) 8 is even, divide by 2 and obtain 4.
+        //  Step 5) 4 is even, divide by 2 and obtain 2.
+        //  Step 6) 2 is even, divide by 2 and obtain 1.
+
+        // Example 2:
+        // Input: s = "10"
+        // Output: 1
+        // Explanation: "10" corressponds to number 2 in their decimal representation.
+        //  Step 1) 2 is even, divide by 2 and obtain 1.
+
+        // Example 3:
+        // Input: s = "1"
+        // Output: 0
+
+        // Constraints:
+        //  - 1 <= s.length <= 500
+        //  - s consists of characters '0' or '1'
+        //  - s[0] == '1'
+
+
+
+        // ---------------------------------------- My Solution ----------------------------------------
+        public int NumSteps(string s)
+        {
+            BigInteger num = 0;
+            var power = 0;
+            for (int i = s.Length - 1; i >= 0; i--)
+                num += (BigInteger)(Char.GetNumericValue(s[i]) * Math.Pow(2, power++));
+
+            //Console.WriteLine(num);
+
+            var steps = 0;
+            while (num != 1)
+            {
+                if (num % 2 == 0)
+                    num /= 2;
+                else
+                    num += 1;
+
+                steps++;
+            }
+
+            return steps;
+        }
+        // 80 ms <-- Beats 78.82%
+        // Solved April 4, 2020
+
+
+
+        // ---------------------------------------- 64 ms Solution on LeetCode ----------------------------------------
+        public int NumSteps64(string s)
+        {
+            if (s.Length == 1)
+                return 0;
+            var steps = 0;
+            var oldOne = 0;
+            var oldOneReal = 0;
+            var onePos = 0;
+            var zeroPos = s.Length;
+            var zeroPosReal = s.Length;
+            var length = s.Length;
+
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (s[i] == '1')
+                {
+                    onePos = i;
+                    break;
+                }
+            }
+
+
+            while (length != 1)
+            {
+                while (zeroPosReal > 0)
+                {
+                    zeroPosReal--;
+                    if (s[zeroPosReal] == '0')
+                    {
+                        zeroPos = zeroPosReal;
+                        break;
+                    }
+
+
+                }
+
+                if (s[zeroPosReal] != '0')
+                {
+                    steps++;
+                    steps += length - oldOne;
+                    break;
+                }
+                else if (zeroPos < onePos)
+                {
+                    steps++;
+                    var shift = onePos - zeroPos;
+                    steps += shift;
+                    length -= shift;
+                    onePos = length - 1;
+                    //oldOne += shift - shift;
+                    oldOneReal += shift;
+
+                }
+                else if (onePos < zeroPos)
+                {
+                    var shift = length - 1 - onePos;
+                    steps += shift;
+                    //oldOne += shift;
+                    length -= shift;
+                    onePos = length - 1;
+                    oldOneReal += shift;
+
+                }
+
+            }
+
+            return steps;
+        }
     }
 }
