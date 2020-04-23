3b22068774a4c5ecb8438fd6f0aec4288079f7de
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-04-23 07:50:22 -0700

    Solved April 22, 2020

---
+++ LC_H_DisplayTableOfFoodOrdersInARestaurant.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPracticeExercises
+{
+    class LC_H_DisplayTableOfFoodOrdersInARestaurant
+    {
+        // ---------------------------------------- Problem ----------------------------------------
+
+        // 1418. Display Table of Food Orders in a Restaurant
+        // Given the array orders, which represents the orders that customers have done in a restaurant.
+        // More specifically orders[i]=[customerNamei, tableNumberi, foodItemi] where customerNamei is the name of the customer, 
+        // tableNumberi is the table customer sit at, and foodItemi is the item customer orders.
+
+
+        // Return the restaurant's “display table”. 
+        // The “display table” is a table whose row entries denote how many of each food item each table ordered. 
+        // The first column is the table number and the remaining columns correspond to each food item in alphabetical order. 
+        // The first row should be a header whose first column is “Table”, followed by the names of the food items. 
+        // Note that the customer names are not part of the table. Additionally, the rows should be sorted in numerically increasing order.
+
+        // Example 1:
+        // Input: orders = [["David", "3", "Ceviche"], ["Corina","10","Beef Burrito"], ["David","3","Fried Chicken"], ["Carla","5","Water"], ["Carla","5","Ceviche"], ["Rous","3","Ceviche"]]
+        // Output: [["Table","Beef Burrito","Ceviche","Fried Chicken","Water"],["3","0","2","1","0"],["5","0","1","0","1"],["10","1","0","0","0"]] 
+        // Explanation:
+        // The displaying table looks like:
+        //      Table,Beef Burrito, Ceviche, Fried Chicken,Water
+        //      3    ,0           ,2      ,1            ,0
+        //      5    ,0           ,1      ,0            ,1
+        //      10   ,1           ,0      ,0            ,0
+        // For the table 3: David orders "Ceviche" and "Fried Chicken", and Rous orders "Ceviche".
+        // For the table 5: Carla orders "Water" and "Ceviche".
+        // For the table 10: Corina orders "Beef Burrito". 
+
+        // Example 2:
+        // Input: orders = [["James","12","Fried Chicken"],["Ratesh","12","Fried Chicken"],["Amadeus","12","Fried Chicken"],["Adam","1","Canadian Waffles"],["Brianna","1","Canadian Waffles"]]
+        // Output: [["Table","Canadian Waffles","Fried Chicken"],["1","2","0"],["12","0","3"]] 
+        // Explanation: 
+        // For the table 1: Adam and Brianna order "Canadian Waffles".
+        // For the table 12: James, Ratesh and Amadeus order "Fried Chicken".
+
+        // Example 3:
+        // Input: orders = [["Laura","2","Bean Burrito"],["Jhon","2","Beef Burrito"],["Melissa","2","Soda"]]
+        // Output: [["Table","Bean Burrito","Beef Burrito","Soda"],["2","1","1","1"]]
+
+        // Constraints:
+        //  - 1 <= orders.length <= 5 * 10^4
+        //  - orders[i].length == 3
+        //  - 1 <= customerNamei.length, foodItemi.length <= 20
+        //  - customerNamei and foodItemi consist of lowercase and uppercase English letters and the space character.
+        //  - tableNumberi is a valid integer between 1 and 500.
+
+
+
+        // ---------------------------------------- My Solution ----------------------------------------
+        public IList<IList<string>> DisplayTable(IList<IList<string>> orders)
+        {
+            var output = new List<IList<string>>();
+            var uniqueTables = new HashSet<int>();
+            var uniqueFoods = new HashSet<string>();
+            foreach (var order in orders)
+            {
+                uniqueTables.Add(Convert.ToInt32(order[1]));
+                uniqueFoods.Add(order[2]);
+            }
+
+            var tables = new int[uniqueTables.Count];
+            uniqueTables.CopyTo(tables);
+            Array.Sort(tables);
+            var foods = new string[uniqueFoods.Count];
+            uniqueFoods.CopyTo(foods);
+            Array.Sort(foods, StringComparer.Ordinal);
+
+            var columns = new List<string>() { "Table" };
+            foreach (var food in foods)
+                columns.Add(food);
+            output.Add(columns);
+
+            for (int i = 0; i < tables.Length; i++)
+            {
+                var temp = new List<string>() { tables[i].ToString() };
+                for (int j = 0; j < foods.Length; j++)
+                    temp.Add("0");
+
+                foreach (var order in orders)
+                {
+                    if (order[1] == tables[i].ToString())
+                    {
+                        var count = temp[Array.IndexOf(foods, order[2]) + 1];
+                        temp[Array.IndexOf(foods, order[2]) + 1] = (Convert.ToInt32(count) + 1).ToString();
+                    }
+                }
+
+                output.Add(temp);
+            }
+
+            return output;
+        }
+        // 1548 ms <-- Beats 5.21%
+        // Solved April 22, 2020
+
+
+
+        // ---------------------------------------- 528 ms Solution on LeetCode ----------------------------------------
+        public IList<IList<string>> DisplayTable528(IList<IList<string>> orders)
+        {
+            // customer => table
+            HashSet<string> foods = new HashSet<string>();
+            Dictionary<int, Dictionary<string, int>> tables = new Dictionary<int, Dictionary<string, int>>();
+
+            foreach (IList<string> order in orders)
+            {
+                string customer = order[0];
+                int table = int.Parse(order[1]);
+                string food = order[2];
+
+                if (!foods.Contains(food))
+                {
+                    foods.Add(food);
+                }
+
+                if (!tables.ContainsKey(table))
+                {
+                    tables[table] = new Dictionary<string, int>();
+                }
+                Dictionary<string, int> currentTable = tables[table];
+
+                if (!currentTable.ContainsKey(food))
+                {
+                    currentTable[food] = 0;
+                }
+                currentTable[food]++;
+            }
+
+            string[] sortedFoods = foods.ToArray();
+            // Array.Sort(sortedFoods, delegate(string a, string b) {
+            //     return ToLower(a).CompareTo(ToLower(b));
+            // });
+            Array.Sort(sortedFoods, delegate (string a, string b) {
+                int i = 0;
+                int j = 0;
+                while (i < a.Length && j < b.Length)
+                {
+                    if (a[i] != b[j])
+                    {
+                        return a[i].CompareTo(b[j]);
+                    }
+                    i++;
+                    j++;
+                }
+
+                if (i >= a.Length && j >= b.Length)
+                {
+                    return 0;
+                }
+                else if (i >= a.Length)
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 1;
+                }
+            });
+
+            IList<IList<string>> result = new List<IList<string>>();
+            result.Add(new List<string>() { "Table" });
+
+            foreach (string food in sortedFoods)
+            {
+                result[0].Add(food);
+            }
+            int[] sortedTables = tables.Keys.ToArray();
+            Array.Sort(sortedTables);
+
+            foreach (int table in sortedTables)
+            {
+                List<string> row = new List<string>() { table.ToString() };
+
+                foreach (string food in sortedFoods)
+                {
+                    row.Add(tables[table].ContainsKey(food) ? tables[table][food].ToString() : "0");
+                }
+
+                result.Add(row);
+            }
+
+            return result;
+        }
+
+        public string ToLower(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                if (c >= 'A' && c <= 'Z')
+                {
+                    c = (char)((int)c - 'A' + 'a');
+                }
+                sb.Append(c);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ LC_S_ReformatTheString.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPracticeExercises
+{
+    class LC_S_ReformatTheString
+    {
+        // ---------------------------------------- Problem ----------------------------------------
+
+        // 1417. Reformat The String
+
+        // Given alphanumeric string s. (Alphanumeric string is a string consisting of lowercase English letters and digits).
+
+        // You have to find a permutation of the string where no letter is followed by another letter and no digit is followed by another digit.
+        // That is, no two adjacent characters have the same type.
+
+        // Return the reformatted string or return an empty string if it is impossible to reformat the string.
+
+        // Example 1:
+        // Input: s = "a0b1c2"
+        // Output: "0a1b2c"
+        // Explanation: No two adjacent characters have the same type in "0a1b2c". "a0b1c2", "0a1b2c", "0c2a1b" are also valid permutations.
+
+        // Example 2:
+        // Input: s = "leetcode"
+        // Output: ""
+        // Explanation: "leetcode" has only characters so we cannot separate them by digits.
+
+        // Example 3:
+        // Input: s = "1229857369"
+        // Output: ""
+        // Explanation: "1229857369" has only digits so we cannot separate them by characters.
+
+        // Example 4:
+        // Input: s = "covid2019"
+        // Output: "c2o0v1i9d"
+
+        // Example 5:
+        // Input: s = "ab123"
+        // Output: "1a2b3"
+
+        // Constraints:
+        //  - 1 <= s.length <= 500
+        //  - s consists of only lowercase English letters and/or digits.
+
+
+
+        // ---------------------------------------- My Solution ----------------------------------------
+        public string Reformat(string s)
+        {
+            var letters = new List<char>();
+            var digits = new List<char>();
+
+            foreach (char c in s)
+            {
+                if (c < 97)
+                    digits.Add(c);
+                else
+                    letters.Add(c);
+            }
+
+            if (Math.Abs(letters.Count - digits.Count) > 1)
+                return "";
+
+            var output = letters.Count > digits.Count ? Merge(letters, digits) : Merge(digits, letters);
+
+            return output;
+        }
+
+        public string Merge(List<char> larger, List<char> smaller)
+        {
+            int i = 0;
+            int pos = 1;
+            while (smaller.Count > 0)
+            {
+                larger.Insert(pos, smaller[0]);
+                pos += 2;
+                smaller.RemoveAt(0);
+            }
+
+            return String.Join("", larger);
+        }
+        // 84 ms <-- Beats 98.06%
+        // Solved April 22, 2020
+
+
+
+        // ---------------------------------------- 80 ms Solution on LeetCode ----------------------------------------
+        public string Reformat80(string s)
+        {
+            StringBuilder letter = new StringBuilder();
+            StringBuilder digit = new StringBuilder();
+
+            foreach (char c in s)
+            {
+                if (IsLetter(c))
+                {
+                    letter.Append(c);
+                }
+                else
+                {
+                    digit.Append(c);
+                }
+            }
+
+            if (Math.Abs(letter.Length - digit.Length) > 1)
+            {
+                return "";
+            }
+
+            StringBuilder max = letter;
+            StringBuilder min = digit;
+            if (letter.Length < digit.Length)
+            {
+                max = digit;
+                min = letter;
+            }
+
+            StringBuilder result = new StringBuilder();
+            for (int i = 0, j = 0; i < max.Length; i++)
+            {
+                result.Append(max[i]);
+                if (j < min.Length)
+                {
+                    result.Append(min[j]);
+                    j++;
+                }
+            }
+
+            return result.ToString();
+        }
+
+        public bool IsLetter(char c)
+        {
+            return c >= 'a' && c <= 'z';
+        }
+    }
+}
